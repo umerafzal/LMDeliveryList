a3b4c57308db55b4ce3d052a9ec4327cd8f60646
Umer Afzal <umer.afzal@foodpanda.com>
2022-06-23 10:45:24 +0500

    Empty files added for VIPER components for delivery list screen.

---
+++ LMDeliveryList/Modules/Delivery/List/Interactor/DeliveryListInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  DeliveryListInteractor.swift
+//  LMDeliveryList
+//
+//  Created by Umer Afzal on 23/06/2022.
+//
+
+import Foundation
---
+++ LMDeliveryList/Modules/Delivery/List/Presenter/DeliveryListPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  DeliveryListPresenter.swift
+//  LMDeliveryList
+//
+//  Created by Umer Afzal on 23/06/2022.
+//
+
+import Foundation
