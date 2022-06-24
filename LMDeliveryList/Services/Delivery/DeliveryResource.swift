08608dc699c5da2f9c1245e2f9c6c192c40c4f56
Umer Afzal <umer.afzal@foodpanda.com>
2022-06-24 05:54:10 +0500

    `DeliveryResource` - deliveries endpoint added with conformance to `ResourceParameter` protocol from Network layer.

--- LMDeliveryList/Services/Delivery/DeliveryResource.swift
+++ LMDeliveryList/Services/Delivery/DeliveryResource.swift
@@ -7,3 +7,41 @@
 
 import Foundation
 
+enum DeliveryResource: ResourceParameters {
+  case fetchDeliveries(offset: Int, limit: Int)
+
+  var path: String {
+    switch self {
+    case .fetchDeliveries(let offset, let limit):
+      return "deliveries?offset=\(offset)&limit=\(limit)"
+    }
+  }
+
+  var httpMethod: HTTPMethod {
+    switch self {
+    case .fetchDeliveries:
+      return .GET
+    }
+  }
+
+  var body: [String : Any]? {
+    switch self {
+    case .fetchDeliveries:
+      return nil
+    }
+  }
+
+  var headers: [String : String]? {
+    switch self {
+    case .fetchDeliveries:
+      return nil
+    }
+  }
+
+  var bodyFormat: HttpBodyFormat {
+    switch self {
+    default:
+        return .URLFormData
+    }
+  }
+}
