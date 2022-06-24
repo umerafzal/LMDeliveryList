2d0967fb0bce388b087b69a40a54d21162243880
Umer Afzal <umer.afzal@foodpanda.com>
2022-06-24 07:42:07 +0500

    DeliveryList presenter injected to DeliveryListController via constructor injection

--- DeliveryListViewController.swift
+++ DeliveryListViewController.swift
@@ -13,6 +13,17 @@ protocol DeliveryListView: AnyObject {
 }
 
 final class DeliveryListViewController: UIViewController {
+  private let presenter: DeliveryListPresenting
+
+  init(presenter: DeliveryListPresenting) {
+    self.presenter = presenter
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
